fb51f059deba812bf00a6ac0d1ac43ea1c9a4118
mindulmindul <mindul@qq.com>
2024-08-18 16:16:50 +0800

    add 256 color for vim-scheme
    Change-Id: Ief1c10aa6636d93672276aff3a70eaa087133fd3

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -x
 mypath=$(dirname $(realpath $0))
 
 if [[ -e ~/.tmux.conf ]]; then
-	mv ~/.tmux.conf ~/.tmux.conf.bak
+    mv ~/.tmux.conf ~/.tmux.conf.bak
 fi
 cp $mypath/.tmux.conf ~/.tmux.conf
 mkdir -p ~/code/.config/tmux/mytools
